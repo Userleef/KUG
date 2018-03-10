2ea2355b1406da0c7a51ba66dd282e73a9fc3250
ininous <ikhemir@epita.fr>
2018-03-10 14:35:57 +0100

    V1.10
    ON A CONNECTE LE MENU AVEC LE JEU ET ON A MIS UN BOUTON RETOUR AU MENU EN JEU

---
+++ Assets/scripts/MANAGE.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MANAGE : MonoBehaviour
+{
+    public void PLAY(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
